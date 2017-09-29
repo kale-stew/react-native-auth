2b187f1055e092f4614d2e45578be82f60b77553
kale-stew <kylieastewart@gmail.com>
2017-09-29 11:07:40 -0700

    init commit

---
+++ notes.js
@@ -0,0 +1,18 @@
+// Auth App
+
+// Overview: 
+    // landing page to log in
+    // create new account if user doesn't already exist 
+    // log out 
+
+// Components:
+    // Header
+    // Card/CardSection
+    // LoginForm
+    // Button
+
+// Anticipated Challenges:
+    // handle text input
+    // validating user data
+    // incorrect password = big error message
+    // attempting to log a user in = display a spinner
