f8502b738f7f433403799087340bdab9a96c3ace
kale-stew <kylieastewart@gmail.com>
2017-09-29 13:16:08 -0700

    notes

--- notes.js
+++ notes.js
@@ -38,3 +38,34 @@ export * from './Button';
     // best time to do it? right before the app renders to the device 
     // dotenv is probably best practice here
     // componentWillMount() { firebase.initializeApp() }
+
+// Handling Text Inputs in React Native
+    // available as a primitive element:
+        import { TextInput } from 'react-native';
+    // defaults to no view, add a height and width to display it
+        // style={{ height: 20, width: 300 }}
+    // onChangeText={ text => this.setState({ text }) }
+        // when text is input...
+        // take that text and...
+        // set the state of 'text' to equal the text input, then...
+        // component rerenders 'this.state.text'
+    // we want Input to receive props of label, value, onChangeText
+        // pass into the functional component as JSX props
+    // refactor LoginForm to adapt to new props and track appropriate states
+        // state = { text: '' } ==> 
+            // state = { email: '', password: '' }
+
+    // Sign In Attempt
+        // ==> Success: cool! 
+        // Fail: 
+            // ==> Create an Account
+                // ==> Success: cool!
+                // Fail:
+                    // ==> Show an Error "try a different email or password"
+
+
+// ActivityIndicator in react-native
+    // props allowed: size (large or small) 
+    // our code: by default, if a prop is not passed, display 'large'
+
+// conditional rendering is best done in helper methods 
